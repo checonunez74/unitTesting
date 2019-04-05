35a5feeae0d0001135e1c852a46a150b1beeb139
Sergio Nunez <checonunez@gmail.com>
2019-04-05 12:51:48 -0500

    updated the code

--- test_calc.py
+++ test_calc.py
@@ -4,5 +4,12 @@ import calculator
 class TestCalc(unittest.TestCase):
 
 	def test_add(self):
-		result = calculator.add(10, 5)
-		self.assertEqual(result, 15)
+		
+		self.assertEqual(calculator.add(10, 5), 15)
+		self.assertEqual(calculator.add(-1, 1), 0)
+		self.assertEqual(calculator.add(-1, -1), -2)
+		
+
+
+if __name__ == '__main__':
+	unittest.main()
